fe228a5adfbf46bb6a20703a5f50000eb7b4f31e
aleksandr <ugolkov.prog@gmail.com>
2025-03-23 14:18:48 +0200

    k8s service

---
+++ k8s/service/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+	router.GET("/", helloHandler)
+	router.GET("/hello", helloDelayHandler)
+	router.GET("/delay", delayHandler)
+
+	err := http.ListenAndServe(":5858", router)
+	if err != nil {
+		fmt.Printf("listen error: %v", err)
+	}
+}
+
+func helloHandler(c *gin.Context) {
+	name := c.DefaultQuery("name", "Guest")
+	c.String(http.StatusOK, "Hello %s!", name)
+}
+
+func helloDelayHandler(c *gin.Context) {
+	name := c.DefaultQuery("name", "Guest")
+
+	delay := rand.Intn(1000)
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+
+	c.String(http.StatusOK, "Hello %s! (%d ms)", name, delay)
+}
+
+func delayHandler(c *gin.Context) {
+	d := c.DefaultQuery("d", "0")
+
+	delay, _ := strconv.Atoi(d)
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+
+	c.String(http.StatusOK, "%d ms", delay)
+}
