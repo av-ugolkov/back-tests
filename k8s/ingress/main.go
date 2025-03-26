d5e555eb3d81adf0ced12e6e08ed3f56b27c9378
aleksandr <ugolkov.prog@gmail.com>
2025-03-26 15:55:04 +0200

    k8s ingress

--- k8s/ingress/main.go
+++ k8s/ingress/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"math/rand"
 	"net/http"
-	"strconv"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+var podName string
+
 func main() {
+	podName = os.Getenv("POD_NAME")
+	fmt.Println("Pod Name:", podName)
+
+	gin.SetMode(gin.ReleaseMode)
+
 	router := gin.New()
 	router.GET("/", helloHandler)
-	router.GET("/hello", helloDelayHandler)
-	router.GET("/delay", delayHandler)
+	router.GET("/hello/delay", helloDelayHandler)
 
 	err := http.ListenAndServe(":5858", router)
 	if err != nil {
@@ -24,7 +31,8 @@ func main() {
 
 func helloHandler(c *gin.Context) {
 	name := c.DefaultQuery("name", "Guest")
-	c.String(http.StatusOK, "Hello %s!", name)
+	slog.Info(fmt.Sprintf("Welcom %s on the pod %s! (%s)", name, podName, c.ClientIP()))
+	c.String(http.StatusOK, "Welcom %s on the pod %s! (%s)", name, podName, c.ClientIP())
 }
 
 func helloDelayHandler(c *gin.Context) {
@@ -33,14 +41,6 @@ func helloDelayHandler(c *gin.Context) {
 	delay := rand.Intn(1000)
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 
-	c.String(http.StatusOK, "Hello %s! (%d ms)", name, delay)
-}
-
-func delayHandler(c *gin.Context) {
-	d := c.DefaultQuery("d", "0")
-
-	delay, _ := strconv.Atoi(d)
-	time.Sleep(time.Duration(delay) * time.Millisecond)
-
-	c.String(http.StatusOK, "%d ms", delay)
+	slog.Info(fmt.Sprintf("Welcom %s on the pod %s! (%d ms) (%s)", name, podName, delay, c.ClientIP()))
+	c.String(http.StatusOK, "Welcom %s on the pod %s! (%d ms) (%s)", name, podName, delay, c.ClientIP())
 }
