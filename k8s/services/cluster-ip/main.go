dc153f2871d808a6f49f6c111fa3f84d4a05d3da
aleksandr <ugolkov.prog@gmail.com>
2025-03-27 16:33:49 +0200

    k8s services cluster-ip

---
+++ k8s/services/cluster-ip/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	// "github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v2"
+)
+
+var podName string
+
+func main() {
+	podName = os.Getenv("POD_NAME")
+	fmt.Println("Pod Name:", podName)
+
+	// gin.SetMode(gin.ReleaseMode)
+	// router := gin.New()
+	// router.SetTrustedProxies(nil) //show a real address by c.ClientIP()
+	// router.GET("/", helloHandler)
+	// router.GET("/hello/delay", helloDelayHandler)
+	// err := http.ListenAndServe(":5858", router)
+	// if err != nil {
+	// 	fmt.Printf("listen error: %v", err)
+	// }
+
+	router := fiber.New()
+	router.Get("/", helloHandler)
+	router.Get("/hello/delay", helloDelayHandler)
+
+	err := router.Listen(":5858")
+	if err != nil {
+		fmt.Printf("listen error: %v", err)
+	}
+}
+
+// func helloHandler(c *gin.Context) {
+func helloHandler(c *fiber.Ctx) error {
+	// name := c.DefaultQuery("name", "Guest")
+	// slog.Info(fmt.Sprintf("Welcom %s on the pod %s! (%s)", name, podName, c.ClientIP()))
+	// c.String(http.StatusOK, "Welcom %s on the pod %s! (%s)", name, podName, c.ClientIP())
+
+	name := c.Query("name", "Guest")
+	s := fmt.Sprintf("Welcom %s on the pod %s! (%s)-(%s)", name, podName, c.Context().LocalIP(), c.Context().RemoteIP())
+	slog.Info(s)
+	return c.Status(http.StatusOK).SendString(s)
+}
+
+// func helloDelayHandler(c *gin.Context) {
+func helloDelayHandler(c *fiber.Ctx) error {
+	delay := rand.Intn(1000)
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+
+	// name := c.DefaultQuery("name", "Guest")
+
+	// slog.Info(fmt.Sprintf("Welcom %s on the pod %s! (%d ms) (%s)", name, podName, delay, c.ClientIP()))
+	// c.String(http.StatusOK, "Welcom %s on the pod %s! (%d ms) (%s)", name, podName, delay, c.ClientIP())
+
+	name := c.Query("name", "Guest")
+	s := fmt.Sprintf("Welcom %s on the pod %s! (%d ms) (%s)-(%s)", name, podName, delay, c.Context().LocalIP(), c.Context().RemoteIP())
+	slog.Info(s)
+	return c.Status(http.StatusOK).SendString(s)
+}
