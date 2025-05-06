ddfc0d064c9e0dacaa7769c53b432457b24d913d
aleksandr <ugolkov.prog@gmail.com>
2025-05-06 13:19:32 +0300

    go slice add_to_slice

--- go/slice/add_to_slice/main.go
+++ go/slice/add_to_slice/main.go
@@ -5,15 +5,20 @@ import "fmt"
 func main() {
 	sl := make([]int, 0, 4)
 	sl = append(sl, 1, 2, 3)
-	sl = addToSlice(sl)
+	sl = addToSlice(sl, 1)
 	fmt.Println(sl)
+
+	slp := make([]int, 0, 4)
+	slp = append(slp, 1, 2, 3)
+	addToSlicePtr(&slp, 1)
+	fmt.Println(slp)
 }
 
-func addToSlice(sl []int) []int {
-	sl = append(sl, 4)
+func addToSlice(sl []int, v int) []int {
+	sl = append(sl, v)
 	return sl
 }
 
-func addToSlicePtr(slp *[]int) {
-	*slp = append(*slp, 4)
+func addToSlicePtr(slp *[]int, v int) {
+	*slp = append(*slp, v)
 }
--- go/slice/add_to_slice/main_test.go
+++ go/slice/add_to_slice/main_test.go
@@ -6,17 +6,17 @@ import (
 
 func Benchmark_addToSlice(b *testing.B) {
 	b.Run("addToSlice", func(b *testing.B) {
+		sl := make([]int, 0, 4)
+		sl = append(sl, 1, 2, 3)
 		for i := 0; i < b.N; i++ {
-			sl := make([]int, 0, 4)
-			sl = append(sl, 1, 2, 3)
-			addToSlice(sl)
+			sl = addToSlice(sl, i)
 		}
 	})
 	b.Run("addToSlicePtr", func(b *testing.B) {
+		sl := make([]int, 0, 4)
+		sl = append(sl, 1, 2, 3)
 		for i := 0; i < b.N; i++ {
-			sl := make([]int, 0, 4)
-			sl = append(sl, 1, 2, 3)
-			addToSlicePtr(&sl)
+			addToSlicePtr(&sl, i)
 		}
 	})
 }
